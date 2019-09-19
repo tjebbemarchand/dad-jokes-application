de020c24788ffc2a2c51468a55838713f1954f4b
Tjebbe <tjebbemarchand@icloud.com>
2019-09-19 16:26:02 +0200

    Implemented localStorage to save the jokes

--- src/JokeList.js
+++ src/JokeList.js
@@ -12,11 +12,15 @@ class JokeList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      jokes: []
+      jokes: JSON.parse(window.localStorage.getItem('jokes') || '[]')
     };
   }
 
-  async componentDidMount() {
+  componentDidMount() {
+    if (this.state.jokes.length === 0) this.getJokes();
+  }
+
+  async getJokes() {
     // Load Jokes
     let jokes = [];
     while (jokes.length < this.props.numJokesToGet) {
@@ -30,6 +34,7 @@ class JokeList extends Component {
     this.setState({
       jokes
     });
+    window.localStorage.setItem('jokes', JSON.stringify(jokes));
   }
 
   handleVote(id, delta) {
