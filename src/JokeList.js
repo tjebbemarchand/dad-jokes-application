e78437dce90229f43c5b7e5922eda0a8faebf081
Tjebbe <tjebbemarchand@icloud.com>
2019-09-19 16:55:23 +0200

    Sort the jokes based on the total votes

--- src/JokeList.js
+++ src/JokeList.js
@@ -78,6 +78,7 @@ class JokeList extends Component {
         </div>
       );
     }
+    let jokes = this.state.jokes.sort((a, b) => b.votes - a.votes);
     return (
       <div className="JokeList">
         <div className="JokeList-sidebar">
@@ -93,7 +94,7 @@ class JokeList extends Component {
           </button>
         </div>
         <div className="JokeList-jokes">
-          {this.state.jokes.map(j => (
+          {jokes.map(j => (
             <Joke
               key={j.id}
               votes={j.votes}
