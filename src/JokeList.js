9d62abd40f1543d083f292e9f182195828c1edb8
Tjebbe <tjebbemarchand@icloud.com>
2019-09-19 16:31:49 +0200

    Fixed localStorage big and made the button work to get new jokes

--- src/JokeList.js
+++ src/JokeList.js
@@ -14,6 +14,8 @@ class JokeList extends Component {
     this.state = {
       jokes: JSON.parse(window.localStorage.getItem('jokes') || '[]')
     };
+
+    this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount() {
@@ -31,16 +33,25 @@ class JokeList extends Component {
       });
       jokes.push({ id: uuid(), text: res.data.joke, votes: 0 });
     }
-    this.setState({
-      jokes
-    });
-    window.localStorage.setItem('jokes', JSON.stringify(jokes));
+    this.setState(
+      st => ({
+        jokes: [...st.jokes, ...jokes]
+      }),
+      () => window.localStorage.setItem('jokes', JSON.stringify(this.state.jokes))
+    );
   }
 
   handleVote(id, delta) {
-    this.setState(st => ({
-      jokes: st.jokes.map(j => (j.id === id ? { ...j, votes: j.votes + delta } : j))
-    }));
+    this.setState(
+      st => ({
+        jokes: st.jokes.map(j => (j.id === id ? { ...j, votes: j.votes + delta } : j))
+      }),
+      () => window.localStorage.setItem('jokes', JSON.stringify(this.state.jokes))
+    );
+  }
+
+  handleClick() {
+    this.getJokes();
   }
 
   render() {
@@ -54,7 +65,9 @@ class JokeList extends Component {
             src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg"
             alt=""
           />
-          <button className="JokeList-getmore">New Jokes</button>
+          <button className="JokeList-getmore" onClick={this.handleClick}>
+            New Jokes
+          </button>
         </div>
         <div className="JokeList-jokes">
           {this.state.jokes.map(j => (
